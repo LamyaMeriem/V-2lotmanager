0928afa30d120349fe3a780ed139a90c78793b59
Lamya Meriem <lamya.meriem@gmail.com>
2025-06-14 12:43:32 +0200

    Update AdminLotManagerController.php

--- controllers/admin/AdminLotManagerController.php
+++ controllers/admin/AdminLotManagerController.php
@@ -3,22 +3,25 @@
  * Lot Manager - Main Admin Controller
  */
 
+require_once _PS_MODULE_DIR_ . 'lotmanager/classes/LotManagerLot.php';
+require_once _PS_MODULE_DIR_ . 'lotmanager/classes/LotManagerDefect.php';
+
 class AdminLotManagerController extends ModuleAdminController
 {
     public function __construct()
     {
         $this->bootstrap = true;
         $this->context = Context::getContext();
-        
+
         parent::__construct();
-        
+
         $this->meta_title = $this->l('Gestionnaire de Lots');
     }
 
     public function initContent()
     {
         parent::initContent();
-        
+
         // Redirection vers le tableau de bord par défaut
         Tools::redirectAdmin($this->context->link->getAdminLink('AdminLotManagerDashboard'));
     }
